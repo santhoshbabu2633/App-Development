edcd402936db758b051768cd896aa4a823febe73
santhoshbabu2633 <120237449+santhoshbabu2633@users.noreply.github.com>
2023-09-27 14:35:09 +0530

    Delete day 11/project directory

--- day 11/project/src/main/java/com/example/demo/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.AdminModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<AdminModel, Long> {
-    AdminModel findByAdminemail(String adminemail);
-}
--- day 11/project/src/main/java/com/example/demo/service/AdminService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.AdminModel;
-
-public interface AdminService {
-    boolean validateLogin(String adminemail, String adminpassword);
-}
