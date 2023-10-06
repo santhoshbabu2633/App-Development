099c2e445c7d48a8ea3b5266b7008881ff99bb6d
santhoshbabu2633 <120237449+santhoshbabu2633@users.noreply.github.com>
2023-10-06 13:47:00 +0530

    Delete day 12/project directory

--- day 12/project/src/main/java/com/example/demo/model/enumerate/Role.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.enumerate;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public enum Role {
-    ADMIN,
-    READER
-}
--- day 12/project/src/main/java/com/example/demo/repository/StoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.demo.model.StoryModel;
-
-@Repository
-public interface StoryRepository extends JpaRepository<StoryModel,Long>{
-
-}
